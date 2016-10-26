2574a81cb51786abd075b555d20fe04b12c3d703
Steve <sloan.steve@gmail.com>
2016-10-26 18:55:28 -0400

    adding gulp for styles

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./themes/hugo-agency-theme/static/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./themes/hugo-agency-theme/static/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./themes/hugo-agency-theme/static/scss/**/*.scss', ['sass']);
+});

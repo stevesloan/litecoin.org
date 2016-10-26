84b8b580738a85f7cf181aa7162b8c10ae230075
Steve <sloan.steve@gmail.com>
2016-10-26 19:07:14 -0400

    fixed naming issue

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./themes/hugo-agency-theme/static/scss/**/*.scss')
+  return gulp.src('./themes/hugo-agency-theme/static/sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./themes/hugo-agency-theme/static/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./themes/hugo-agency-theme/static/scss/**/*.scss', ['sass']);
+  gulp.watch('./themes/hugo-agency-theme/static/sass/**/*.sass', ['sass']);
 });

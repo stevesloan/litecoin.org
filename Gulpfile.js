4fa315b0532032dcd5fbdaf1c0a6c4a71178c534
Steve <sloan.steve@gmail.com>
2016-10-26 22:30:03 -0400

    fixed mobile nav colors

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,10 +2,14 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./themes/hugo-agency-theme/static/sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['> 5%']
+    }))
     .pipe(gulp.dest('./themes/hugo-agency-theme/static/css'));
 });
 

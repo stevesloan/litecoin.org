793217126ff2e25bfd124b325fd08505315e19c8
Steve Sloan <sloan.steve@gmail.com>
2016-10-28 09:42:20 -0400

    renamed theme name

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,15 +5,15 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-  return gulp.src('./themes/hugo-agency-theme/static/sass/**/*.sass')
+  return gulp.src('./themes/hugo-litecoin-theme/static/sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
         browsers: ['> 5%']
     }))
-    .pipe(gulp.dest('./themes/hugo-agency-theme/static/css'));
+    .pipe(gulp.dest('./themes/hugo-litecoin-theme/static/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./themes/hugo-agency-theme/static/sass/**/*.scss', ['sass']);
-  gulp.watch('./themes/hugo-agency-theme/static/sass/**/*.sass', ['sass']);
+  gulp.watch('./themes/hugo-litecoin-theme/static/sass/**/*.scss', ['sass']);
+  gulp.watch('./themes/hugo-litecoin-theme/static/sass/**/*.sass', ['sass']);
 });

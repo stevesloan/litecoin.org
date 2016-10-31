e8bed52cc5f2226b31356e821dbae18613b58ff3
Steve <sloan.steve@gmail.com>
2016-10-30 23:13:06 -0400

    breaking apart config into partials

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
@@ -13,7 +14,14 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./themes/hugo-litecoin-theme/static/css'));
 });
 
+gulp.task('config', function() {
+  return gulp.src(['./config_partials/base.toml', './config_partials/*.toml'])
+    .pipe(concat('config.all.toml'))
+    .pipe(gulp.dest('.'));
+});
+
 gulp.task('sass:watch', function () {
   gulp.watch('./themes/hugo-litecoin-theme/static/sass/**/*.scss', ['sass']);
   gulp.watch('./themes/hugo-litecoin-theme/static/sass/**/*.sass', ['sass']);
+  gulp.watch('./config_partials/**/*/toml', ['config']);
 });

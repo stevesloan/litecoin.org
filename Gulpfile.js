e9a74fcc4b6a29f9c6a254667aa8775447ae47a7
Steve <sloan.steve@gmail.com>
2016-10-30 23:28:26 -0400

    finished separating config files

--- Gulpfile.js
+++ Gulpfile.js
@@ -16,12 +16,12 @@ gulp.task('sass', function () {
 
 gulp.task('config', function() {
   return gulp.src(['./config_partials/base.toml', './config_partials/*.toml'])
-    .pipe(concat('config.all.toml'))
+    .pipe(concat('config.toml'))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./themes/hugo-litecoin-theme/static/sass/**/*.scss', ['sass']);
   gulp.watch('./themes/hugo-litecoin-theme/static/sass/**/*.sass', ['sass']);
-  gulp.watch('./config_partials/**/*/toml', ['config']);
+  gulp.watch('./config_partials/**/*.toml', ['config']);
 });
